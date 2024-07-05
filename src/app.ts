fb7c9cf44b35c0c7f08b09de5629efb836499f60
wenbo.yang <wenbo.yang.84@gmail.com>
2024-07-04 23:07:07 -0700

    add function

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
 import { NeuralNetwork } from "brain.js";
 import { INeuralNetworkData, INeuralNetworkDatum } from "brain.js/dist/neural-network";
+import { convertBitmapDataToZeroOneMat } from './Chisel-Global-Common-Libraries/src/lib/binaryMatUtils';
 import Jimp from "jimp";
 
 async function readTrainingData(): Promise<Jimp[]> {
-    throw new Error('Not Implemented');
+    const trainingDataImages = [];
+    for (let i = 0; i < 18; i++) {
+        const url = `./trainingData/character_zou/zou_charactrer_prepared_${i.toString()}_test.png`;
+        trainingDataImages.push(await Jimp.read(url));
+    }
+
+    return trainingDataImages;
 }
 
-async function readAndConvertTrainingData(): Promise<number[][]> {
-    throw new Error('Not Implemented');
+async function readAndConvertTrainingData(trainingDataImages: Jimp[]): Promise<number[][]> {
+    const trainingDataNumber: number[][] = [];
+
+    for (let i = 0; i < trainingDataImages.length; i++) {
+        trainingDataNumber.push([]);
+        const bmpBuffer = await trainingDataImages[i].getBufferAsync(Jimp.MIME_BMP)
+        const mat = await convertBitmapDataToZeroOneMat(bmpBuffer, 250);
+
+        const flattenedData = mat.flat(1);
+        trainingDataNumber.push(flattenedData);
+    }
+
+    return trainingDataNumber;
 }
 
 async function readTestData(): Promise<Jimp[]> {
@@ -24,7 +42,6 @@ async function trainModel(data: number[][]): Promise<NeuralNetwork<INeuralNetwor
     const trainingData: Array<INeuralNetworkDatum<INeuralNetworkData, INeuralNetworkData>> = [];
 
     for (let i = 0; i < data.length; i++) {
-
             let output: any = {};
             output['走'] = 1;
             let input: INeuralNetworkData = data[i];
@@ -40,12 +57,11 @@ async function runTestDataThroughNeuralNetwork(net: NeuralNetwork<INeuralNetwork
 }
 
 
-const data = await readAndConvertTrainingData();
-const net = await trainModel(data)
+const trainingImages = await readTrainingData();
+const trainingData = await readAndConvertTrainingData(trainingImages);
+const net = await trainModel(trainingData);
 const testData = await readAndConvertTestData();
 const result = await runTestDataThroughNeuralNetwork(net, testData);
 
 console.log(result);
-
-
 
